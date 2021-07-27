6b1d21aa68b752e796db6361a02f1688cf5e50e4
Chris Yang <50881233+ChrisY0910@users.noreply.github.com>
2021-07-27 14:16:58 +0000

    Created a simple program to calculate averages and bill totals.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #Chris Yang 7/27/21 Python practice on Repl
+import turtle
+
+#Problem 2: Averages
+#Ask for numbers
+num1 = int(input("Give me the first number."))
+num2 = int(input("Give me the second number."))
+num3 = int(input("Give me the third number."))
+num4 = int(input("Give me the fourth number."))
+#Finds the average
+print("This is your average: " + str((num1 + num2 + num3 + num4) / 4))
+
+
+#Problem 3: Dinner bill
+#Ask user for total cost of dinner.
+total = int(input("What is your total bill?:"))
+#Asks user how many people were there.
+diners = int(input("How many diners ate this meal?:"))
+#Calculate and print how much each person should pay
+print("This is how much each person should pay: $" + str(total/diners))
